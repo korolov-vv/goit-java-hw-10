47d3ca7d8236a36010f8b0f6c05458f1df2bfb58
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-03-05 01:42:19 +0100

    update

--- Exercise5.java
+++ Exercise5.java
@@ -1,16 +1,9 @@
-import java.util.Arrays;
+import java.util.*;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Exercise5 {
-    public static void main(String[] args) {
-        Stream<String> s1 = Arrays.stream(new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K"});
-        Stream<String> s2 = Arrays.stream(new String[]{"L", "M", "N", "O", "P", "Q", "R"});
-        System.out.println(zip(s1, s2).collect(Collectors.joining(" ")));
-    }
 
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second){
 
--- Test.java
+++ Test.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) {
@@ -9,6 +12,8 @@ public class Test {
         long c = 11;
         long m = (long) Math.pow(2, 48);
         long limit = 10;
+        Stream<String> s1 = Arrays.stream(new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K"});
+        Stream<String> s2 = Arrays.stream(new String[]{"L", "M", "N", "O", "P", "Q", "R"});
 
         System.out.println("Exercise 1 result:");
         System.out.println(new Exercise1().filter(names));
@@ -19,10 +24,14 @@ public class Test {
         System.out.println("Exercise 3 result:");
         new Exercise3().sortNumbers(numbers);
 
-        System.out.println();
         System.out.println("Exercise 4 result:");
+        System.out.println();
         new Exercise4().randomStream(0, a, c, m)
                 .limit(limit)
                 .forEach((l) -> System.out.print(l.toString().concat(", ")));
+
+        System.out.println("Exercise 5 result:");
+        System.out.println();
+        System.out.println(new Exercise5().zip(s1, s2).collect(Collectors.joining(" ")));
     }
 }
